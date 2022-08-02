c202c97559fd63d428b373e826abf4940926a67c
raluchukwu mbaebie <raluchukwu.mbaebie@gmail.com>
2022-08-02 08:13:41 +0100

    Fix Middleware exception

--- LockManagementSystem.Application/Middlewares/ExceptionMiddleware.cs
+++ LockManagementSystem.Application/Middlewares/ExceptionMiddleware.cs
@@ -26,36 +26,45 @@ public class ExceptionMiddleware
         var responseModel = new ResponseModel<string>();
         try
         {
-            await _next(httpContext);
+            await _next.Invoke(httpContext);
         }
         catch (BadRequestException badRequestException)
         {
             _logger.LogError(badRequestException, Constants.ExceptionHandlerMessage);
             response.StatusCode = (int) HttpStatusCode.BadRequest;
             responseModel.Message = badRequestException.Message;
+            
+            await WriteResponse(response, responseModel);
         }
         catch (NotFoundException notFoundException)
         {
             _logger.LogError(notFoundException, Constants.ExceptionHandlerMessage);
             response.StatusCode = (int) HttpStatusCode.NotFound;
             responseModel.Message = notFoundException.Message;
+            
+            await WriteResponse(response, responseModel);
         }
         catch (UnauthorizedAccessException unauthorizedAccessException)
         {
             _logger.LogError(unauthorizedAccessException, "Unauthorized Access");
             response.StatusCode = (int) HttpStatusCode.Unauthorized;
             responseModel.Message = unauthorizedAccessException.Message;
+            
+            await WriteResponse(response, responseModel);
         }
         catch (Exception exception)
         {
             _logger.LogError(exception, Constants.ExceptionHandlerMessage);
             response.StatusCode = (int) HttpStatusCode.InternalServerError;
             responseModel.Message = "Server error.";
+
+            await WriteResponse(response, responseModel);
         }
-        finally
-        {
-            var responseString = JsonConvert.SerializeObject(responseModel);
-            await response.WriteAsync(responseString);
-        }
+    }
+
+    private async Task WriteResponse(HttpResponse response, ResponseModel<string> responseModel)
+    {
+        var responseString = JsonConvert.SerializeObject(responseModel);
+        await response.WriteAsync(responseString);
     }
 }
